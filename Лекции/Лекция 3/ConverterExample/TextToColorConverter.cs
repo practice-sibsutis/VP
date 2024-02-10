5abdad82c32a66d8e151b6e3282b03760fc1caf6
QQQ <QQQ>
2024-02-10 16:07:15 +0700

    Add converter example

---
+++ Лекции/Лекция 3/ConverterExample/TextToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace ConverterExample;
+
+public class TextToColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string text && targetType.IsAssignableFrom(typeof(IBrush)))
+        {
+            Color.TryParse(text, out var color);
+            return new SolidColorBrush(color);
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
