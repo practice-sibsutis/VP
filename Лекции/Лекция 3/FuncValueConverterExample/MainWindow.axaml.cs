7cfaa4922fc269385185beb0c94e31dea6d4ea47
QQQ <QQQ>
2024-02-10 16:24:02 +0700

    Functional converter example

---
+++ Лекции/Лекция 3/FuncValueConverterExample/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FuncValueConverterExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Лекции/Лекция 3/FuncValueConverterExample/TextToColorConverter.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace FuncValueConverterExample;
+
+public static class TextToColorConverter
+{
+    public static FuncValueConverter<string?, IBrush> ToColorConverter { get; } =
+        new FuncValueConverter<string?, IBrush>(text =>
+        {
+            if (Color.TryParse(text, out Color color))
+            {
+                return new SolidColorBrush(color);
+            }
+
+            return null;
+        });
+}
