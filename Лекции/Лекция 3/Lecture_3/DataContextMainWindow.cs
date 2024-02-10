c1f6012d3248b9a5d1c62a344b7ad4e540091beb
QQQ <QQQ>
2024-02-10 14:24:40 +0700

    Create a program context for MainWindow

---
+++ Лекции/Лекция 3/Lecture_3/DataContextMainWindow.cs
@@ -0,0 +1,14 @@
+﻿namespace Lecture_3;
+
+public class DataContextMainWindow
+{
+    private string _text = "Hello World";
+
+    public string Text
+    {
+        get
+        {
+            return _text;
+        }
+    }
+}
