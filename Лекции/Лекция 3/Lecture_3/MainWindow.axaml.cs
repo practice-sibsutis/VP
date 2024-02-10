84737d4fadb5301869b4c485b12ca935fea94729
QQQ <QQQ>
2024-02-10 14:18:14 +0700

    Create Avalonia app

---
+++ Лекции/Лекция 3/Lecture_3/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Lecture_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
